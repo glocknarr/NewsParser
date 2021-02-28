3aca024c64ee9e83f7c72c2287c3068859ffa5c9
Руслан Давлетшин <glocknarr@gmail.com>
2021-03-01 00:13:51 +0300

    Новости парсятся из rss

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
+import datetime
+
 
 site_link = "https://www.fontanka.ru/"
 rss_link = "https://www.fontanka.ru/fontanka.rss"
@@ -6,5 +9,24 @@ rss_link = "https://www.fontanka.ru/fontanka.rss"
 response = requests.get(rss_link)
 status = response.status_code  # получаем статус запроса. Если 200, то ОК
 text = response.text  # получаем текст страницы
-print(status)
+soup = BeautifulSoup(text, 'lxml')
+item_list = soup.find_all("item")
+for item in item_list:
+    # print(item)
+    title = item.find("title").text
+    description = item.find("description").text
+    uid = item.find("guid").text
+    news_link = item.find("pdalink").text
+    category = item.find("category").text
+    pubDate = item.find('pubdate').text
+    print(title)
+    if description != '':
+        print("\t" + description)
+    if news_link != "":
+        print(news_link)
+    print("\tid = " + uid + ", категория: " + category)
+    print("\t\tДата публикации: " + str(pubDate))
+    print("==========================================")
+
+# print(status)
 # print(text)
